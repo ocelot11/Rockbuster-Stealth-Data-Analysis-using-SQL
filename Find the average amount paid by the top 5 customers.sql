aedfa23e6a23141a0369ce3ce5ea8af195ab60d1
ocelot11 <126712813+ocelot11@users.noreply.github.com>
2023-08-19 23:54:59 +0200

    Update and rename CTE.sql to Find the average amount paid by the top 5 customers.sql

--- Find the average amount paid by the top 5 customers.sql
+++ Find the average amount paid by the top 5 customers.sql
@@ -1,5 +1,5 @@
-# 1. Find the average amount paid by the top 5 customers.
-
+# CTE
+	
 WITH total_amount_paid AS
 	(SELECT customer.customer_id AS Customer_ID, 
 		   customer.first_name AS Customer_First_Name,
@@ -20,9 +20,10 @@ WITH total_amount_paid AS
 SELECT AVG(total_amount_paid.Total_Amount_Paid) AS average
 FROM total_amount_paid
 
-# 2. Find out how many of the top 5 customers are based within each country.
+# SUBQUERY
 
-WITH top_5_customers AS
+SELECT AVG(total_amount_paid.Total_Amount_Paid) AS average
+FROM
 	(SELECT customer.customer_id AS Customer_ID, 
 		   customer.first_name AS Customer_First_Name,
 		   customer.last_name AS Customer_Last_Name,
@@ -37,16 +38,6 @@ WITH top_5_customers AS
 	WHERE city.city_id IN (42, 312, 401, 179, 197, 43, 307, 216, 339, 252)
 	GROUP BY customer.customer_id, country.country, city.city
 	ORDER BY Total_Amount_Paid DESC
-	LIMIT 5)
+	LIMIT 5) AS total_amount_paid
 
-SELECT country.country,
-	   COUNT(DISTINCT customer.customer_id) AS all_customer_count,
-	   COUNT(DISTINCT top_5_customers) AS top_customer_count
-FROM customer
-INNER JOIN address ON customer.address_id = address.address_id
-INNER JOIN city ON address.city_id = city.city_id
-INNER JOIN country ON city.country_id = country.country_id
-LEFT JOIN top_5_customers ON customer.customer_id = top_5_customers.customer_id
-GROUP BY country.country
-HAVING COUNT(DISTINCT top_5_customers) > 0
-ORDER BY top_customer_count DESC
+	
