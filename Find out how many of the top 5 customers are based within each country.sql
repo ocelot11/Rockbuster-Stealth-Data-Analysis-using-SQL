f539248e40518a7dd4eb88999079316501ff530f
ocelot11 <126712813+ocelot11@users.noreply.github.com>
2023-08-19 23:55:07 +0200

    Update and rename Subqueries.sql to Find out how many of the top 5 customers are based within each country.sql

--- Find out how many of the top 5 customers are based within each country.sql
+++ Find out how many of the top 5 customers are based within each country.sql
@@ -1,7 +1,6 @@
-# Find the average amount paid by the top 5 customers.
+# CTE
 
-SELECT AVG(total_amount_paid.Total_Amount_Paid) AS average
-FROM
+WITH top_5_customers AS
 	(SELECT customer.customer_id AS Customer_ID, 
 		   customer.first_name AS Customer_First_Name,
 		   customer.last_name AS Customer_Last_Name,
@@ -16,10 +15,23 @@ FROM
 	WHERE city.city_id IN (42, 312, 401, 179, 197, 43, 307, 216, 339, 252)
 	GROUP BY customer.customer_id, country.country, city.city
 	ORDER BY Total_Amount_Paid DESC
-	LIMIT 5) AS total_amount_paid
+	LIMIT 5)
 
-# Find out how many of the top 5 customers are based within each country.
+SELECT country.country,
+	   COUNT(DISTINCT customer.customer_id) AS all_customer_count,
+	   COUNT(DISTINCT top_5_customers) AS top_customer_count
+FROM customer
+INNER JOIN address ON customer.address_id = address.address_id
+INNER JOIN city ON address.city_id = city.city_id
+INNER JOIN country ON city.country_id = country.country_id
+LEFT JOIN top_5_customers ON customer.customer_id = top_5_customers.customer_id
+GROUP BY country.country
+HAVING COUNT(DISTINCT top_5_customers) > 0
+ORDER BY top_customer_count DESC
 
+
+# SUBQUERIES
+	
 SELECT country.country,
 	   COUNT(DISTINCT customer.customer_id) AS all_customer_count,
 	   COUNT(DISTINCT top_5_customers) AS top_customer_count
