80960e5b8a66771e53c514b9a5d865018d75fc7d
ocelot11 <126712813+ocelot11@users.noreply.github.com>
2023-08-19 23:51:28 +0200

    Create TOP 10 CITIES WITHIN THE TOP 10 COUNTRIES.sql

---
+++ TOP 10 CITIES WITHIN THE TOP 10 COUNTRIES.sql
@@ -0,0 +1,8 @@
+SELECT country.country, city.city_id, city.city, COUNT(customer.customer_id) AS number_of_customers
+FROM customer
+INNER JOIN address ON customer.address_id = address.address_id
+INNER JOIN city ON address.city_id = city.city_id
+INNER JOIN country ON city.country_id = country.country_id
+GROUP BY country.country, city.city_id 
+ORDER BY number_of_customers DESC
+LIMIT 10;
